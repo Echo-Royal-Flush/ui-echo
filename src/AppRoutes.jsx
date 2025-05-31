9e48a715c60508cc1b113741139b1ba18301058b
PedroRamos <phramosaraujo02@gmail.com>
2025-05-31 05:08:37 -0300

    add

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -2,13 +2,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import { LoginPage } from './pages/LoginPage/LoginPage'
 import { AdminTeams } from './pages/AdminTeams/AdminTeams'
+import { GameHub } from './pages/GameHub/GameHub'
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<LoginPage />}></Route>
-        <Route path='' element >
+        <Route path='/gamehub' element={<GameHub />} >
         </Route>
         <Route path='/teams' element={<AdminTeams />}></Route>
       </Routes>
--- src/pages/GameHub/GameHub.jsx
+++ src/pages/GameHub/GameHub.jsx
@@ -1,9 +1,14 @@
 import { Box } from "@mui/material"
+import { Header } from "../../components/Header/Header"
+import PlayCard from "../../components/PlayCard/PlayCard"
 
 export const GameHub = () => {
     return (
         <Box>
-            aaaaa
+            <Header />
+            <Box>
+                <PlayCard>aperta ae</PlayCard>
+            </Box>
         </Box>
     )
 }
