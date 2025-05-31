4022ad820bc0fcdd48c0ddd0d58a93d99811f343
PedroRamos <phramosaraujo02@gmail.com>
2025-05-31 03:13:30 -0300

    att

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,16 +1,67 @@
 import { Box } from "@mui/material"
+import { useState } from "react";
 
 export const LoginPage = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (!email || !password) {
+            setError('Preencha todos os campos.');
+            return;
+        }
+
+        console.log('Email:', email);
+        console.log('Senha:', password);
+        setError('');
+
+        // Chamar  API
+        alert('Login enviado!');
+    };
     return (
         <Box sx={{
             backgroundColor: '#B655F5',
             display: 'flex',
-            alignContent: 'center',
-            height: '100vh'
+            height: '100vh',
+            textAlign: 'center',
+            justifyContent: 'center'
         }}>
-           <Box>
-            aaaa
-            </Box> 
+            <Box>
+                <form onSubmit={handleSubmit} 
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'column'
+                    }}>
+                    <h2>Echo</h2>
+
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        style={{
+
+                        }}
+                    />
+
+                    <input
+                        type="password"
+                        placeholder="Senha"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        style={{
+
+                        }}
+                    />
+
+                    {error && <p style={{}}>{error}</p>}
+
+                    <button type="submit" style={{}}>Entrar</button>
+                </form>
+            </Box>
         </Box>
     )
 }
